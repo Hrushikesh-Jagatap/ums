a2c85829e3e07e0cc7a266a99d2de56987d920e4
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-14 15:48:44 +0530

    Update index.js

--- src/apis/controllers/v1/index.js
+++ src/apis/controllers/v1/index.js
@@ -3,11 +3,12 @@ const UsersController = require('./Users');
 const EmployeeController = require('./Employee');
 const FavouriteController = require('./Favourite');
 const ConsumerController = require('./Consumer');
-
+const HomeController = require('./HomeController')
 module.exports = {
   OtpController,
   UsersController,
   EmployeeController,
   FavouriteController,
   ConsumerController,
+  HomeController,
 };
