21acb08d1ab4172e1d45e3fe26c81490cc44dd0e
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-14 15:50:13 +0530

    Update index.js

--- src/apis/models/index.js
+++ src/apis/models/index.js
@@ -4,7 +4,8 @@ const Address = require('./Address');
 const Favourite = require('./Favourite');
 const Organization = require('./Organization');
 const Consumer = require('./Consumer');
-
+const PrivateTution = require('./PrivateTut');
+const HomeTuition = require('./HomeTut');
 module.exports = {
   Employee,
   Users,
@@ -12,4 +13,6 @@ module.exports = {
   Favourite,
   Organization,
   Consumer,
+    PrivateTution,
+  HomeTuition
 };
